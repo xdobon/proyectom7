12988e2c715af053f0e9a202e91fc0b3b4dc77da
Xavi Dobon <x.dobon10@gmail.com>
2019-12-12 09:12:24 +0100

    Actividad1

--- ProjecteM7/app/src/main/java/com/dobon/projectm7/Registre.java
+++ ProjecteM7/app/src/main/java/com/dobon/projectm7/Registre.java
@@ -3,11 +3,7 @@ package com.dobon.projectm7;
 import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
-import android.os.Bundle;
-import android.view.Gravity;
-import android.view.View;
-import android.widget.Toast;
+
 
 public class Registre extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
