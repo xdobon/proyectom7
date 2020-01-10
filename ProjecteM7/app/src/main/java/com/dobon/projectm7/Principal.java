1d95e1591508b63d1d6bca61538e36c82b0fc47f
Xavi Dobon <x.dobon10@gmail.com>
2020-01-10 09:53:28 +0100

    Actividad1

--- ProjecteM7/app/src/main/java/com/dobon/projectm7/Principal.java
+++ ProjecteM7/app/src/main/java/com/dobon/projectm7/Principal.java
@@ -1,8 +1,12 @@
 package com.dobon.projectm7;
 
 import android.os.Bundle;
+import android.widget.Toolbar;
 
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 public class Principal extends AppCompatActivity {
 
@@ -10,5 +14,8 @@ public class Principal extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.principal);
+
     }
-}
+
+    }
+
