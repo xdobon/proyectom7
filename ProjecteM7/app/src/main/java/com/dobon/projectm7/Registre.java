294d6ba7fe24a2e0b8d1c72b055dedfcb64cc55b
Xavi Dobon <x.dobon10@gmail.com>
2020-01-20 11:56:55 +0100

    Actividad1

--- ProjecteM7/app/src/main/java/com/dobon/projectm7/Registre.java
+++ ProjecteM7/app/src/main/java/com/dobon/projectm7/Registre.java
@@ -6,7 +6,12 @@ import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.Gravity;
 import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.SeekBar;
+import android.widget.Spinner;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -20,6 +25,12 @@ public class Registre extends AppCompatActivity {
     EditText cognom;
     EditText username;
     EditText contrasenya;
+    RadioButton home;
+    RadioButton dona;
+    SeekBar seekBar;
+    Spinner sp_frase;
+    CheckBox checkBox;
+    Button btn_registrar;
     SharedPreferences prefs ;
 
     @Override
@@ -36,12 +47,23 @@ public class Registre extends AppCompatActivity {
         cognom = findViewById(R.id.txt_cognom);
         username = findViewById(R.id.txt_username);
         contrasenya = findViewById(R.id.txt_contrasenya);
+        home = findViewById(R.id.rb_home);
+        dona= findViewById(R.id.rb_dona);
+        seekBar= findViewById(R.id.seekBar);
+        sp_frase= findViewById(R.id.sp_frase);
+        checkBox= findViewById(R.id.checkBox);
+
 
         SharedPreferences.Editor editor = prefs.edit();
         editor.putString("nom", String.valueOf(nom));
         editor.putString("cognom", String.valueOf(cognom));
         editor.putString("user", String.valueOf(username));
         editor.putString("password", String.valueOf(contrasenya));
+      /*  editor.putBoolean("home", Boolean.valueOf(home));
+        editor.putBoolean("dona", Boolean.valueOf(dona));
+        editor.putBoolean("seekBar", Boolean.valueOf(seekBar));
+        editor.putBoolean("sp_frase", Boolean.valueOf(sp_frase));
+        editor.putBoolean("checkBox", Boolean.valueOf(checkBox));*/
         editor.commit();
 
     }
