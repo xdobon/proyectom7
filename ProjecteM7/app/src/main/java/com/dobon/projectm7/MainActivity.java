6995e16226ee9c74f99f7993c9fe33efa4c9d274
Xavi Dobon <x.dobon10@gmail.com>
2020-01-29 11:36:10 +0100

    Actividad1

--- ProjecteM7/app/src/main/java/com/dobon/projectm7/MainActivity.java
+++ ProjecteM7/app/src/main/java/com/dobon/projectm7/MainActivity.java
@@ -5,12 +5,15 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.content.res.Configuration;
 import android.os.Bundle;
 import android.view.Gravity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
+
+import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -73,6 +76,35 @@ public class MainActivity extends AppCompatActivity {
         editor.apply();
 
     }
-    
+    public void cambiaCat(View view) {
+        Locale catalan = new Locale("ca", "CA");
+        Locale.setDefault(catalan);
+        Configuration config = new Configuration();
+        config.locale = catalan;
+        getBaseContext().getResources().updateConfiguration(config, getBaseContext().getResources().getDisplayMetrics());
+        Intent cat = new Intent(getApplicationContext(), MainActivity.class);
+        cat.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        startActivity(cat);
+    }
+    public void cambiaEsp(View view) {
+        Locale espanol = new Locale("es", "ES");
+        Locale.setDefault(espanol);
+        Configuration config = new Configuration();
+        config.locale = espanol;
+        getBaseContext().getResources().updateConfiguration(config, getBaseContext().getResources().getDisplayMetrics());
+        Intent esp = new Intent(getApplicationContext(), MainActivity.class);
+        esp.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        startActivity(esp);
+    }
+    public void cambiaEn(View view) {
+        Locale ingles = new Locale("en", "ES");
+        Locale.setDefault(ingles);
+        Configuration config = new Configuration();
+        config.locale = ingles;
+        getBaseContext().getResources().updateConfiguration(config, getBaseContext().getResources().getDisplayMetrics());
+        Intent ing = new Intent(getApplicationContext(), MainActivity.class);
+        ing.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        startActivity(ing);
+    }
 
 }
